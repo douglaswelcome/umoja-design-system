20754b0dc321d6b5a3dacef0a3a47c8f75b437f0
Douglas <douglaswelcome@gmail.com>
2022-06-20 14:03:42 -0700

    update logo

--- .storybook/sama-theme.js
+++ .storybook/sama-theme.js
@@ -4,5 +4,5 @@ export default create({
   base: 'dark',
   brandTitle: 'Sama',
   brandUrl: 'https://www.sama.com/',
-  brandImage: 'https://aesimpson.github.io/umoja-design-system/images/logo-white.png',
+  brandImage: './static_assets/img/logo.svg',
 });
